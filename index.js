7a844401b87e82e72fecdfbd900ba9643533fa65
heyvats21 <81029204+heyvats21@users.noreply.github.com>
2021-07-29 22:18:53 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-//with the help of const the value can't be redeclare
-const play1= Math.floor(Math.random() *6 )+1;
-// const play1dice = "images/dice" + play1 +".png";
-const play1dice = `images/dice${play1}.png`
-
-document.getElementById('check1').setAttribute('src',play1dice)
